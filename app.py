36b9d16e693cc56ed224dd0a04127fb3a01c28ec
horlando <orland20161@gmail.com>
2022-05-03 19:01:34 -0500

    se finalizo ejercicio

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
+from datetime import timedelta, date
+
+def daterange(start_date, end_date):
+        for n in range(int ((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+
+def meses(mes):
+    if mes=='Jan':
+        return 1
+    elif mes=='Feb':
+        return 2
+    elif mes=='Mar':
+        return 3
+    elif mes=='Apr':
+        return 4
+    elif mes=='May':
+        return 5
+    elif mes=='Jun':
+        return 6
+    elif mes=='Jul':
+        return 7
+    elif mes=='Aug':
+        return 8
+    elif mes=='Sep':
+        return 9
+    elif mes=='Oct':
+        return 10
+    elif mes=='Nov':
+        return 11
+    elif mes=='Dec':
+        return 12
+
 def completar(a):
-    mes=a[0].split(" ")[0]
-    year=a[0].split(" ")[2]
-    minimo_dia=int(a[0].split(" ")[1].split(',')[0])
-    maximo_dia=int(a[-1].split(" ")[1].split(',')[0])
-    salida=[]
-    for i in range(minimo_dia,maximo_dia+1):
-         salida.append(mes+" "+str(i)+", "+year)
-    print (salida)
+    mes_inicial=meses(a[0].split(" ")[0])
+    mes_final=meses(a[-1].split(" ")[0])
+    year_incial=int(a[0].split(" ")[2])
+    year_final=int(a[-1].split(" ")[2])
+    dia_inicial=int(a[0].split(" ")[1].split(',')[0])
+    dia_final=int(a[-1].split(" ")[1].split(',')[0])
+    start_date = date(year_incial, mes_inicial, dia_inicial)
+    end_date = date(year_final, mes_final, dia_final+1)
+    fechas=[]
+    for single_date in daterange(start_date, end_date):
+        fechas.append(single_date.strftime("%b %d, %Y"))
 
-completar(['Oct 2, 1969', 'Oct 5, 1969', 'Oct 7, 1969', 'Oct 9, 1969'])
+    print(fechas)
 
-
-# se realizo ejercicio que completa fechas para un mismo mes de un determinado año
+completar(['Nov 2, 1969','Dec 9, 1969'])
